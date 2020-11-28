018867944298446f13c63c1811a946b9259b76f4
Guy Erb <guyerb@me.com>
2020-11-27 22:25:35 -0800

    size bucket logs - complete with crash

--- src/dmalloc_log.c
+++ src/dmalloc_log.c
@@ -20,8 +20,31 @@ int dmalloc_is_logging = 1;
    where noted)
 */
 
+/* find range (powers of 10) of age buckets with largest value
+   N.B - assume that len is a power of 10 (i.e. 10, 100, 1000...)
+ */
+static int logline_power10_largest(uint32_t *p, size_t len)
+{
+  uint32_t power = 0;
+  uint32_t ceiling = 1;
+  uint32_t floor = ceiling;
+  uint32_t largest = 0;
+
+ for (power = 1; power <= len; power *= 10) {;}
+
+  for (uint32_t i = 0; i < power; i++) {
+    uint32_t count = 0;
+    ceiling = ceiling * 10;
+    for (floor = ceiling; floor < ceiling; floor++) {
+      count += p[floor];
+    }
+    if (count > largest) largest = count;
+  }
+  return largest;
+}
+
 /* find size bucket with most bytes, assuming max bytes per bucket */
-static uint32_t logline_largest(uint32_t *p, size_t len)
+static uint32_t logline_power2_largest(uint32_t *p, size_t len)
 {
   uint32_t largest = 0;
   uint8_t exp = 2;		/* start on the 0-4 bucket  */
@@ -46,7 +69,7 @@ static uint32_t logline_scaler(uint32_t largest, uint32_t columns)
 }
 
 /* a little recursive char '#' scaled dumper */
-static void logline_print_scaled(char *hdr, uint32_t count, uint32_t scale)
+static void logline_scaled(char *hdr, uint32_t count, uint32_t scale)
 {
   if (count < scale) {
     dmalloc_printf("%15s: ", hdr);
@@ -57,21 +80,31 @@ static void logline_print_scaled(char *hdr, uint32_t count, uint32_t scale)
       if (count >= scale/2) 	/* round */
 	dmalloc_logf("%11s:#\n", hdr);
       else
-	logline_print_scaled(hdr, count - scale, scale);
+	logline_scaled(hdr, count - scale, scale);
     }
   } else {
     dmalloc_logf("#");
   }
 }
 
+static void logline_range_scaled(char *hdr, uint32_t *p, uint32_t floor, uint32_t ceiling, uint32_t scale)
+{
+  uint32_t count = 0;
+
+  for (int i = floor; floor < ceiling; i++) {
+    count += p[i];
+  }
+  logline_scaled(hdr, count, scale);
+}
+
 /* a little recursive formatter from the interwebz cuz "%'d" didn't work*/
-void logline_print_commas (uint32_t n)
+void logline__commas (uint32_t n)
 {
     if (n < 1000) {
       dmalloc_logf ("%d", n);
         return;
     }
-    logline_print_commas(n/1000);
+    logline__commas(n/1000);
     dmalloc_logf (",%03d", n%1000);
 }
 
@@ -103,10 +136,10 @@ void dmalloc_stats_log()
   dmalloc_logf("========== %s: UTC %s ==========\n", DMALLOC_VERSION_STRING, pgm_str);
   dmalloc_logf("Dmalloc stats:\n");
   dmalloc_logf("%-30s", "overall allocations:" );
-  logline_print_commas(stats.s_allocated_alltime);
+  logline__commas(stats.s_allocated_alltime);
   dmalloc_logf("\n");
   dmalloc_logf("%-30s", "current allocations:" );
-  logline_print_commas(stats.s_allocated_current);
+  logline__commas(stats.s_allocated_current);
   dmalloc_logf("\n");
 
 #ifdef DMALLOC_STATS_UNIT
@@ -120,23 +153,33 @@ void dmalloc_stats_log()
 #endif	/* DMALLOC_STATS_UNIT */
 
   /* dump age buckets */
-  uint32_t largest = logline_largest( (uint32_t *)&stats.s_sizebuckets, BUCKETS_SIZE_NUM);
-  uint32_t scaler = logline_scaler(largest, 65);
+  uint32_t sz_largest = logline_power2_largest( (uint32_t *)&stats.s_sizebuckets, BUCKETS_SIZE_NUM);
+  uint32_t sz_scaler = logline_scaler(sz_largest, 65);
 
-  dmalloc_logf("Current size allocations by bytes: ( one # represents %d bytes)\n", scaler);
+  dmalloc_logf("Current size allocations by bytes: ( one # represents %d bytes)\n", sz_scaler);
 
-  logline_print_scaled("0 - 4",		stats.s_sizebuckets[BUCKET_0000], scaler);
-  logline_print_scaled("4 - 8",		stats.s_sizebuckets[BUCKET_0004], scaler);
-  logline_print_scaled("8 - 16",	stats.s_sizebuckets[BUCKET_0008], scaler);
-  logline_print_scaled("16 - 32",	stats.s_sizebuckets[BUCKET_0016], scaler);
-  logline_print_scaled("32 - 64",	stats.s_sizebuckets[BUCKET_0032], scaler);
-  logline_print_scaled("64 - 128",	stats.s_sizebuckets[BUCKET_0064], scaler);
-  logline_print_scaled("128 - 256",	stats.s_sizebuckets[BUCKET_0128], scaler);
-  logline_print_scaled("256 - 512",	stats.s_sizebuckets[BUCKET_0256], scaler);
-  logline_print_scaled("512 - 1024",	stats.s_sizebuckets[BUCKET_0512], scaler);
-  logline_print_scaled("1024 - 2048",	stats.s_sizebuckets[BUCKET_1024], scaler);
-  logline_print_scaled("2048 - 4096",	stats.s_sizebuckets[BUCKET_2048], scaler);
-  logline_print_scaled("4096 - infi",	stats.s_sizebuckets[BUCKET_4096], scaler);
+  logline_scaled("0 - 4",		stats.s_sizebuckets[BUCKET_0000], sz_scaler);
+  logline_scaled("4 - 8",		stats.s_sizebuckets[BUCKET_0004], sz_scaler);
+  logline_scaled("8 - 16",	stats.s_sizebuckets[BUCKET_0008], sz_scaler);
+  logline_scaled("16 - 32",	stats.s_sizebuckets[BUCKET_0016], sz_scaler);
+  logline_scaled("32 - 64",	stats.s_sizebuckets[BUCKET_0032], sz_scaler);
+  logline_scaled("64 - 128",	stats.s_sizebuckets[BUCKET_0064], sz_scaler);
+  logline_scaled("128 - 256",	stats.s_sizebuckets[BUCKET_0128], sz_scaler);
+  logline_scaled("256 - 512",	stats.s_sizebuckets[BUCKET_0256], sz_scaler);
+  logline_scaled("512 - 1024",	stats.s_sizebuckets[BUCKET_0512], sz_scaler);
+  logline_scaled("1024 - 2048",	stats.s_sizebuckets[BUCKET_1024], sz_scaler);
+  logline_scaled("2048 - 4096",	stats.s_sizebuckets[BUCKET_2048], sz_scaler);
+  logline_scaled("4096 - infi",	stats.s_sizebuckets[BUCKET_4096], sz_scaler);
+
+  /* dump size buckets */
+  uint32_t age_largest = logline_power10_largest( (uint32_t *)&stats.s_agebuckets, BUCKETS_AGE_NUM);
+  uint32_t age_scaler = logline_scaler(age_largest, 65);
+
+  dmalloc_logf("Current age allocations by bytes: ( one # represents %d bytes)\n", age_scaler);
+  logline_range_scaled("< 10 sec",    stats.s_sizebuckets, 0, 9, age_scaler);
+  logline_range_scaled("< 100 sec",   stats.s_sizebuckets, 10, 99, age_scaler);
+  logline_range_scaled("< 1000 sec",  stats.s_sizebuckets, 100, 999, age_scaler);
+  logline_range_scaled(">= 1000 sec", stats.s_sizebuckets, 999, 1000, age_scaler);
 
   dmalloc_is_logging = 0;  
 
@@ -153,9 +196,14 @@ void dmalloc_stats_log()
 /* 32 - 64 bytes: */
 /* 64 - 128 bytes: */
 /* 128 - 256 bytes: */
-/* 256 - 512 bytes: ## 512 - 1024 bytes: # 1024 - 2048 bytes: # 2048 - 4096 bytes: # 4096 + bytes: ####### */
+/* 256 - 512 bytes: # */
+/* 512 - 1024 bytes: # */
+/* 1024 - 2048 bytes: # */
+/*  2048 - 4096 bytes: # */
+/* 4096 + bytes: ####### */
 /* Current allocations by age: ( # = 8,123 current allocations) < 1 sec: ### */
 /* < 10 sec: ## */
 /* < 100 sec: ## */
-/* < 1000 sec: ######################### > 1000 sec: */
+/* < 1000 sec: ######################### */
+/* > 1000 sec: */
 }
